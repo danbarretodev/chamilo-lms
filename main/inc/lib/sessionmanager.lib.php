b9ac299bed334e45fc38dc30a1e78f3f30e876ab
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2014-12-11 10:36:52 -0500

    Allow tracking students by boss - refs BT#9071

--- main/inc/lib/sessionmanager.lib.php
+++ main/inc/lib/sessionmanager.lib.php
@@ -4730,7 +4730,7 @@ class SessionManager
                 null,
                 $active,
                 $lastConnectionDate,
-                COURSEMANAGER,
+                api_is_student_boss() ? STUDENT_BOSS : COURSEMANAGER,
                 $keyword
             );
         }
