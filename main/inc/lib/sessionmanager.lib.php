b0cfcd15a9b361d656280c1e1fa92009b3e8016b
Daniel Barreto <daniel.barreto@beeznest.com>
2015-01-15 17:01:40 -0500

    Add description to brief sessions

--- main/inc/lib/sessionmanager.lib.php
+++ main/inc/lib/sessionmanager.lib.php
@@ -5357,7 +5357,7 @@ class SessionManager
                 $joinTable,
                 array(
                     'where' => array(
-                        'sf.field_variable IN ( ? ) OR' => 'modalidad, duracion, cupos, horario, publico_objetivo',
+                        'sf.field_variable IN ( ? ) OR' => 'as_description, modalidad, duracion, cupos, horario, publico_objetivo',
                     )
                 )
             );
@@ -5368,6 +5368,9 @@ class SessionManager
             if ($publicoObjetivo == $session['publico_objetivo']) {
                 $session['modalidad'] = isset($sessionFieldValueList[$id]) ?
                     $sessionFieldValueList[$id]['modalidad'] :
+                    '';
+                $session['descripcion'] = isset($sessionFieldValueList[$id]) ?
+                    $sessionFieldValueList[$id]['as_description'] :
                     '';
                 $session['duracion'] = isset($sessionFieldValueList[$id]) ?
                     $sessionFieldValueList[$id]['duracion'] :
